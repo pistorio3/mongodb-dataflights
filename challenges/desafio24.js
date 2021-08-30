31fd7ab8ae682f55ba4786e0eb3a711319622b87
João Victor Pistorio <joao.pistorio@gmail.com>
2021-08-30 12:17:39 -0300

    Desafio 24

---
+++ challenges/desafio24.js
@@ -0,0 +1,5 @@
+db.voos.find({ $and: [
+  { litrosCombustivel: { $lt: 600 } },
+  { "empresa.nome": { $ne: { $in: ["GOL", "AZUL"] } } },
+  { litrosCombustivel: { $exists: true } },
+  ] }, { _id: 0, vooId: 1, "empresa.nome": 1, litrosCombustivel: 1 }).limit(1);
